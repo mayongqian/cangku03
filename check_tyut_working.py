797768978e11362a3dab2a616fdb4c67575f1ebd
mayongqian <39235014+mayongqian@users.noreply.github.com>
2018-06-07 13:50:56 +0800

    Update check_tyut_working.py

--- check_tyut_working.py
+++ check_tyut_working.py
@@ -17,7 +17,7 @@ def run():
 
         if page.status_code == 200 and page.content.decode("utf-8").find('<title>太原理工大学-教务处</title>') >= 0:
 
-            print(" is working great :)")
+            print(" TYUT is working great :)")
 
         else:
 
