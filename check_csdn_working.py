2faa2f7be64cfd609407d32d693364a5d2ca2305
mayongqian <39235014+mayongqian@users.noreply.github.com>
2018-06-07 13:36:37 +0800

    Update check_csdn_working.py

--- check_csdn_working.py
+++ check_csdn_working.py
@@ -29,7 +29,7 @@ def run():
 
         print("It looks like CSDN is having trouble, some one please take a look at it")
         yag = yagmail.SMTP(user = '1321692006@qq.com', password = 'nwblnqbqvbdfijji', host = 'smtp.qq.com')
-        yag.send(to = ['2802370278@qq.com'], subject = '异常', contents = ['aaa'])
+        yag.send(to = ['2802370278@qq.com'], subject = '构建异常', contents = ['主页被篡改，请及时处理'])
         sys.exit(-1)
 if __name__ == '__main__':
 
