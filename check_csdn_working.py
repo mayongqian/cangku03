b9917db7ca683f2f719b8e86f9b635295d00556a
mayongqian <39235014+mayongqian@users.noreply.github.com>
2018-06-07 13:04:29 +0800

    Update check_csdn_working.py

--- check_csdn_working.py
+++ check_csdn_working.py
@@ -14,7 +14,7 @@ def run():
 
         page = requests.get(WEB_URL)
 
-        if page.status_code == 200 and page.content.decode("utf-8").find('<title>CSDN-专业的IT技术社区</title>') >= 0:
+        if page.status_code == 200 and page.content.decode("utf-8").find('<title>CSDN.NET - CSDN-专业IT技术社区</title>') >= 0:
 
             print("CSDN is working great :)")
 
@@ -22,14 +22,14 @@ def run():
 
             print("It looks like CSDN is having trouble, some one please take a look at it")
 
+            yag = yagmail.SMTP(user = '1321692006@qq.com', password = 'nwblnqbqvbdfijji', host = 'smtp.qq.com')
+            yag.send(to = ['2802370278@qq.com'], subject = '网页服务', contents = ['服务异常，主页被篡改'])
             sys.exit(-1)
 
     except:
 
         print("It looks like CSDN is having trouble, some one please take a look at it")
 
-        yag = yagmail.SMTP(user = '1321692006@qq.com', password = 'nwblnqbqvbdfijji', host = 'smtp.qq.com')
-        yag.send(to = ['2802370278@qq.com'], subject = 'qwq', contents = ['uu'])
         sys.exit(-1)
 
 if __name__ == '__main__':
