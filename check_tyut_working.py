9b68162d792d6a260fc6df13705a164c4ebafdef
mayongqian <39235014+mayongqian@users.noreply.github.com>
2018-06-07 14:02:44 +0800

    Update check_tyut_working.py

--- check_tyut_working.py
+++ check_tyut_working.py
@@ -30,7 +30,7 @@ def run():
 
         print("It looks like TYUT is having trouble, some one please take a look at it")
         yag = yagmail.SMTP(user = '1321692006@qq.com', password = 'nwblnqbqvbdfijji', host = 'smtp.qq.com')
-        yag.send(to = ['2802370278@qq.com'], subject = '异常', contents = ['The TYUT home page is tampered with, please handle it in time'])
+        yag.send(to = ['2802370278@qq.com'], subject = '异常', contents = ['The TYUT home page is changed, please handle it in time'])
         sys.exit(-1)
 if __name__ == '__main__':
 
