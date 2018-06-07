d2e1920ca162e8755c86c4270f658d14bd118ce1
mayongqian <39235014+mayongqian@users.noreply.github.com>
2018-06-07 13:55:39 +0800

    Update check_tyut_working.py

--- check_tyut_working.py
+++ check_tyut_working.py
@@ -15,7 +15,7 @@ def run():
 
         page = requests.get(WEB_URL)
 
-        if page.status_code == 200 and page.content.decode("utf-8").find('<title>太原理DE 工大学-教务处</title>') >= 0:
+        if page.status_code == 200 and page.content.decode("utf-8").find('<title>太原理工大学-教务处</title>') >= 0:
 
             print(" TYUT is working great :)")
 
@@ -30,7 +30,7 @@ def run():
 
         print("It looks like TYUT is having trouble, some one please take a look at it")
         yag = yagmail.SMTP(user = '1321692006@qq.com', password = 'nwblnqbqvbdfijji', host = 'smtp.qq.com')
-        yag.send(to = ['2802370278@qq.com'], subject = '异常', contents = ['The home page is tampered with, please handle it in time'])
+        yag.send(to = ['2802370278@qq.com'], subject = '异常', contents = ['The TYUT home page is tampered with, please handle it in time'])
         sys.exit(-1)
 if __name__ == '__main__':
 
