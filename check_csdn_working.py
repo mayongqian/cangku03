24eb33041d704050ef09985f3cae13e427ef93c9
mayongqian <39235014+mayongqian@users.noreply.github.com>
2018-06-07 12:55:08 +0800

    Add files via upload

---
+++ check_csdn_working.py
@@ -0,0 +1,37 @@
+import requests
+import sys
+import yagmail
+
+
+
+
+WEB_URL = 'http://www.csdn.net/'
+
+
+def run():
+
+    try:
+
+        page = requests.get(WEB_URL)
+
+        if page.status_code == 200 and page.content.decode("utf-8").find('<title>CSDN.NET - CSDN-专业IT技术社区</title>') >= 0:
+
+            print("CSDN is working great :)")
+
+        else:
+
+            print("It looks like CSDN is having trouble, some one please take a look at it")
+
+            sys.exit(-1)
+
+    except:
+
+        print("It looks like CSDN is having trouble, some one please take a look at it")
+
+        yag = yagmail.SMTP(user = '1321692006@qq.com', password = 'nwblnqbqvbdfijji', host = 'smtp.qq.com')
+        yag.send(to = ['2802370278@qq.com'], subject = 'qwq', contents = ['uu'])
+        sys.exit(-1)
+
+if __name__ == '__main__':
+
+    run()
