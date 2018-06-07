da8911bcc362426eb9d0d37bdf7aaf62419b1dfc
mayongqian <39235014+mayongqian@users.noreply.github.com>
2018-06-07 13:35:56 +0800

    Update check_csdn_working.py

--- check_csdn_working.py
+++ check_csdn_working.py
@@ -22,15 +22,14 @@ def run():
 
             print("It looks like CSDN is having trouble, some one please take a look at it")
 
-            yag = yagmail.SMTP(user = '1321692006@qq.com', password = 'nwblnqbqvbdfijji', host = 'smtp.qq.com')
-            yag.send(to = ['2802370278@qq.com'], subject = '异常', contents = ['异常'])
+            
             sys.exit(-1)
 
     except:
 
         print("It looks like CSDN is having trouble, some one please take a look at it")
-        #yag = yagmail.SMTP(user = '1321692006@qq.com', password = 'nwblnqbqvbdfijji', host = 'smtp.qq.com')
-        #yag.send(to = ['2802370278@qq.com'], subject = '异常', contents = ['aaa'])
+        yag = yagmail.SMTP(user = '1321692006@qq.com', password = 'nwblnqbqvbdfijji', host = 'smtp.qq.com')
+        yag.send(to = ['2802370278@qq.com'], subject = '异常', contents = ['aaa'])
         sys.exit(-1)
 if __name__ == '__main__':
 
