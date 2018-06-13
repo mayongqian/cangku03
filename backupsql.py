d0c6011024f48394f9d03ee34355028870ffcecd
mayongqian <39235014+mayongqian@users.noreply.github.com>
2018-06-13 17:08:33 +0800

    sql-backup

---
+++ backupsql.py
@@ -0,0 +1,14 @@
+import pymysql
+import os
+connect = pymysql.connect(       
+    user="root",
+    password="1234",
+    host="127.0.0.1",
+    db="project",
+    charset="utf8",
+    port=3306
+    )
+conn = connect.cursor()
+key="1234"
+path = "/Bakeup"
+os.system("mysql -u root -p%s keshe project> %s.sql" % (key,path))
